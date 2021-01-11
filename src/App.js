c86d85249fe295ae094c381be79fbc884a6b2015
isharrma <54413011+isharrma@users.noreply.github.com>
2021-01-12 00:56:15 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./Button"
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const App = () => {
+    return (
+      <div>
+        <h1>Helllo World</h1>
+        <Button title="Play Store" />
+        <Button title="App Store" />      
+      </div>
+    );
+  }
+
+  export default App;
